47287224d435089cdbb0e77b73326a0511e10fe5
TaliaFlores <talia30flores@gmail.com>
2023-10-12 03:04:58 -0400

    log cmabios

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ const port = 3000 || process.env.PORT;
 // Definir una ruta de prueba
 app.get('/', (req, res) => {
 
-  res.send("#########################");
-  res.send("###### ailatdev REST00 ##");
-  res.send("#########################");
+  res.send("Hola !");
+
 });
 
 // Iniciar el servidor talia
 app.listen(port, () => {
+  console.log("############################");
+  console.log("###### ailatdev REST 00 ####");
+  console.log("############################");
   console.log(`Servidor escuchando en http://localhost:${port}`);
+  
 });
